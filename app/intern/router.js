3b92714f86b1f28a9c253d43ae9fa48176af8db6
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2024-07-22 12:58:26 +0800

    fix: edit router get detail in interns router

--- app/intern/router.js
+++ app/intern/router.js
@@ -6,11 +6,11 @@ const { index, viewDetail, actionStatus } = require('./controller')
 
 /* GET home page. */
 router.get('/', index);
-router.get('/detail/:id', viewDetail);
+router.get('/detail/:id', multer({ dest: os.tmpdir() }).single('photo_profile'), viewDetail);
+// router.get('/detail/:id', viewDetail);
 router.put('/status/:id', actionStatus)
 // router.get('/create', viewCreate);
 // router.post('/create', multer({ dest: os.tmpdir() }).single('photo_profile'), actionCreate);
-// router.get('/detail/:id', multer({ dest: os.tmpdir() }).single('photo_profile'), viewDetail);
 // router.delete('/delete/:id', actionDelete);
 
 module.exports = router;
