a3ab8449f9bd30d16364f9ab1f80821c4226b4ee
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2024-07-22 12:54:34 +0800

    feat: add actionStatus method in intern controller

--- app/intern/controller.js
+++ app/intern/controller.js
@@ -38,4 +38,36 @@ module.exports = {
       res.redirect('/intern')
     }
   },
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      let intern = await Intern.findOne({ _id: id })
+
+      let status = intern.status === 'Y' ? 'N' : 'Y';
+      let statusIntern = 'active';
+
+      if (Date.now() >= Date(intern.start_an_internship)) {
+        let status = intern.status === 'Y' ? 'N' : 'Y';
+        let statusIntern = 'active';
+        // await Supervisor.findOneAndUpdate({ _id: id }, { status })
+
+        req.flash('alertMessage', 'Akun Peserta Telah Aktif');
+        req.flash('alertStatus', 'success');
+
+        res.redirect('/intern')
+
+      } else {
+        req.flash('alertMessage', `Akun tidak bisa diaktifkan. Sesuaikan dengan tanggal mulai magang`);
+        req.flash('alertStatus', 'danger');
+        res.redirect('/intern')
+      }
+
+
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/inten')
+    }
+  },
 };
--- app/intern/router.js
+++ app/intern/router.js
@@ -2,15 +2,15 @@ var express = require('express');
 const multer = require('multer')
 const os = require('os')
 var router = express.Router();
-const { index, viewDetail } = require('./controller')
+const { index, viewDetail, actionStatus } = require('./controller')
 
 /* GET home page. */
 router.get('/', index);
 router.get('/detail/:id', viewDetail);
+router.put('/status/:id', actionStatus)
 // router.get('/create', viewCreate);
 // router.post('/create', multer({ dest: os.tmpdir() }).single('photo_profile'), actionCreate);
 // router.get('/detail/:id', multer({ dest: os.tmpdir() }).single('photo_profile'), viewDetail);
 // router.delete('/delete/:id', actionDelete);
-// router.put('/status/:id', actionStatus)
 
 module.exports = router;
