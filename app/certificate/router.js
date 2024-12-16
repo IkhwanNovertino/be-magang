5e40c3d277e3a119e9e64b7342a2355ca78d3535
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2024-12-16 13:09:54 +0800

    feat: feature-certificate

--- app/certificate/router.js
+++ app/certificate/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { index, template } = require('./controller');
+const { index, template, viewCertificate, downloadFileCertificate } = require('./controller');
 
 // const { isLoginUser } = require('../middleware/auth');
 /* GET home page. */
 router.get('/', index);
+router.get('/create', viewCertificate);
 router.get('/template', template);
+router.get('/download/:file', downloadFileCertificate);
 
 module.exports = router;
